b92238329be5dbc6ac9cb196490be9db5b03beaf
Sylvie ADONSOU <adonsousylvie67@gmail.com>
2025-03-26 17:34:03 +0000

    Mon projet de gestion de bibliotheque

--- backend/config.php
+++ backend/config.php
@@ -1,34 +1,61 @@
 <?php
 
-class database {
-    private $host = "localhost";  
-    private $dbname = "bibliotheque";  
-    private $user = "root";  
-    private $password = ""; 
-    private $pdo;  
+class database{
+
+    private $dbFile = "biblitheque.db";
+    private $pdo;
+
+    public function __construct(){
 
 
-    public function __construct() {
-        try {
-            
-            $this->pdo = new PDO(
-                "mysql:host=" . $this->host . ";dbname=" . $this->dbname, 
-                $this->user, 
-                $this->password
-            );
 
+        try{
+            $this->pdo = new PDO("sqlite" . this->$dbFile);
+            $this->pdo->setAttribute(PDO :: ATTR_ERRMODE, PDO :: ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e){
+            die("Erreur de connexion: " . $e->getMessage());
         
-            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-        } catch (PDOException $e) {
-        
-
-            die("Erreur de connexion : " . $e->getMessage());
         }
     }
 
-    public function getConnection() {
+    public function getConnection(){
         return $this->pdo;
     }
 
+
+    private function createTable() {
+    $sql = "CREATE TABLE books (
+        id INTEGER PRIMARY KEY,
+        title TEXT NOT NULL,
+        author TEXT NOT NULL,
+        published_at DATE NOT NULL,
+        available BOOLEAN DEFAULT 1
+    )";
+    
+
+
+    try {
+
+        $this->pdo->exec($sql);
+        echo "Base de données et table créée avec succès ";
+    }catch (PDOException $e) {
+
+      die ("Eururr au moment de la creation de la table :" . $e->getMessage());
+
+
+
+    }
+    
+
+
+
+
+}
+
+
+
+
+
+}
 
